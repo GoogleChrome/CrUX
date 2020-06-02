e870b8b4bdbb153aa42cc0ed29344c4233d158bb
Shane Exterkamp <shaneexterkamp5@gmail.com>
2020-06-02 16:49:00 -0700

    Add CrUX API node cli lib.

---
+++ demos/api/core_web_vitals/README.md
@@ -0,0 +1,26 @@
+# CrUX Core Web Vitals Checker
+
+## Make and API key
+
+In order to use this tool, you'll need an API key to access the Chrome UX Report API.
+
+You can create a key in the Google API credentials page: https://console.developers.google.com/apis/credentials
+
+## Call the CLI
+
+Send query!
+
+```sh
+$ node main.js --origin "https://example.com" --key yourAPIKey
+```
+
+Get results!
+
+```sh
+Origin: https://example.com
+Metric "cumulative_layout_shift" passes the Core Web Vitals assessment. (0.04 <= 0.1)
+Metric "first_contentful_paint" passes the Core Web Vitals assessment. (2071 <= 2500)
+Metric "first_input_delay" passes the Core Web Vitals assessment. (21 <= 100)
+
+Core Web Vitals Asseesment: PASSED
+```
---
+++ demos/api/core_web_vitals/main.js
@@ -0,0 +1,182 @@
+const fetch = require('node-fetch');
+const chalk = require('chalk');
+var argv = require('yargs').options({
+  'o': {
+    alias: 'origin',
+    describe: 'The origin to lookup.',
+    type: 'string'
+  },
+  'u': {
+    alias: 'url',
+    describe: 'The URL to lookup.',
+    type: 'string'
+  }, 'f': {
+    alias: 'formFactor',
+    describe: 'The form factor to lookup, leave blank to lookup all form factors.',
+    type: 'string',
+    choices: ['PHONE', 'DESKTOP', 'TABLET'],
+  }, 'k': {
+    alias: 'key',
+    demandOption: true,
+    describe: 'The API key to use with the request.',
+    type: 'string',
+  }}).argv;
+
+// Setup the API Util object.
+const CrUXApiUtil = {};
+
+// Setup the API key.
+if (!argv.key) {
+  console.log('An API Key is required to use the Chrome UX Report API.');
+  return;
+}
+CrUXApiUtil.KEY = argv.key;
+
+// Create request to send to API.
+let request = {}
+
+// Set the identifier.
+if (argv.origin) {
+  request.origin = argv.origin;
+} else if (argv.url) {
+  request.url = argv.url;
+} else {
+  console.log('A URL or origin identifier is required.');
+  return;
+}
+
+// Set optional form factor.
+if (argv.formFactor) {
+  request.formFactor = argv.formFactor;
+}
+
+/**
+ * Query the CrUX API for metric records.
+ *
+ * @param {{origin?: string, url?: string, formFactor?: string,  metrics?: Array<string>}} requestBody
+ * return {{record: {key: Object, metrics: Map<string, {histogram: Array<{ start: number, end?: number, density: number}>}>}, urlNormalizationDetails: Object}} */
+CrUXApiUtil.query = async function (requestBody) {
+  const endpointUrl = 'https://chromeuxreport.googleapis.com/v1/records:queryRecord';
+  const resp = await fetch(`${endpointUrl}?key=${CrUXApiUtil.KEY}`, {
+    method: 'POST',
+    body: JSON.stringify(requestBody),
+  });
+
+  const json = await resp.json();
+  if (!resp.ok) {
+    console.log(chalk.red(json.error.message));
+    if (resp.status === 404) {
+      // Extra Not Found flavor text.
+      console.log('Data cannot be found for this URL. ' +
+        'Check that the URL you\'re looking up is correct, ' +
+        'don\'t forget "www" if you need it!');
+    }
+    process.exit(1);
+  }
+  return json;
+};
+
+/**
+ * Utility method to transform the response's metric data into an array of usable metric objects
+ * Example return value:
+  [
+    {
+      "acronym": "FCP",
+      "name": "first_contentful_paint",
+      "labeledBins": [{
+        "label": "good",
+        "percentage": 43.51,
+        "start": 0,
+        "end": 1000,
+        "density": 0.4351
+      }, // ... (other bins) ]
+      "percentiles": {
+          "p75": 1792
+      },
+    }, // ... (other metrics) ]
+*
+* @return {{acronym: string, name: string, labelsBins: Array<{label: 'good'|'needs improvement'|'poor', percentage: number, start: number, end?: number, density: number}>}}*/
+function labelMetricData(metrics) {
+  const nameToAcronymMap = {
+    first_contentful_paint: 'FCP',
+    largest_contentful_paint: 'LCP',
+    first_input_delay: 'FID',
+    cumulative_layout_shift: 'CLS',
+  };
+
+  return Object.entries(metrics).map(([metricName, metricData]) => {
+    const standardBinLabels = ['good', 'needs improvement', 'poor'];
+    const metricBins = metricData.histogram;
+
+    // We assume there are 3 histogram bins and they're in order of: good => poor
+    const labeledBins = metricBins.map((bin, i) => {
+      // Assign a good/poor label, calculate a percentage, and add retain all existing bin properties
+      return {
+        label: standardBinLabels[i],
+        percentage: bin.density * 100,
+        ...bin,
+      };
+    });
+
+    return {
+      acronym: nameToAcronymMap[metricName],
+      name: metricName,
+      labeledBins,
+      percentiles: {
+          p75: metricData.percentiles.p75,
+      },
+    };
+  });
+}
+
+// Gather the data and display it.
+(async function () {
+  const json = await CrUXApiUtil.query(request);
+
+  const labeledMetrics = await labelMetricData(json.record.metrics);
+
+  // The CWV 75%ile thresholds.
+  const nameToCWVMap = {
+    first_contentful_paint: 2500,
+    first_input_delay: 100,
+    cumulative_layout_shift: '0.1',
+  };
+
+  // Display metric results
+  let resId = '';
+  let identifier = '';
+  if (json.record.key.origin) {
+    resId += 'Origin: ' + json.record.key.origin;
+    identifier = json.record.key.origin;
+  } else {
+    resId += 'Url: ' + json.record.key.url;
+    identifier = json.record.key.url;
+  }
+  if (json.record.key.formFactor) {
+    resId += ', FormFactor: ' + json.record.key.formFactor;
+  }
+  // Add form factor too...
+  console.log('\n' + resId);
+  let pass = true;
+  for (const metric of labeledMetrics) {
+    // console.log("Metric: " + JSON.stringify(metric, null, 2));
+    if (nameToCWVMap[metric.name]) {
+        // Check pass fail
+        if (metric.percentiles.p75 <= nameToCWVMap[metric.name]) {
+            console.log('Metric "' + metric.name + '" ' + chalk.green('passes') + ' the Core Web Vitals assessment. (' + metric.percentiles.p75 + ' <= ' + nameToCWVMap[metric.name] + ')');
+        } else {
+            console.log('Metric "' + metric.name + '" ' + chalk.red('failed') + ' the Core Web Vitals assessment. (' + metric.percentiles.p75 + ' !<= ' + nameToCWVMap[metric.name] + ')');
+            pass = false;
+        }
+    }
+  }
+  // The final result for this query.
+  // If everything passed, then CWV passes, otherwise fail!
+  let result = 'Core Web Vitals Assessment: ';
+  if (pass) {
+    result += chalk.green('PASSED');
+  } else {
+    result += chalk.red('FAILED');
+  }
+  console.log('\n' + result);
+})();
