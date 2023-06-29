6a97a151653c5e9dce58d700561088b8e322b16b
Rick Viscomi <rviscomi@users.noreply.github.com>
2023-06-28 20:08:21 -0400

    History API

--- js/crux-api-util.js
+++ js/crux-api-util.js
@@ -5,14 +5,32 @@ const CrUXApiUtil = {};
 // Get your CrUX API key at https://goo.gle/crux-api-key.
 CrUXApiUtil.API_KEY = '[YOUR_API_KEY]';
 CrUXApiUtil.API_HOST = 'https://chromeuxreport.googleapis.com';
+CrUXApiUtil.HISTORY_API_ENDPOINT_PATH = `/v1/records:queryHistoryRecord?key=${CrUXApiUtil.API_KEY}`;
+CrUXApiUtil.HISTORY_API_ENDPOINT = `${CrUXApiUtil.API_HOST}${CrUXApiUtil.HISTORY_API_ENDPOINT_PATH}`;
 CrUXApiUtil.API_ENDPOINT_PATH = `/v1/records:queryRecord?key=${CrUXApiUtil.API_KEY}`;
 CrUXApiUtil.API_ENDPOINT = `${CrUXApiUtil.API_HOST}${CrUXApiUtil.API_ENDPOINT_PATH}`;
 CrUXApiUtil.API_BATCH_ENDPOINT = `${CrUXApiUtil.API_HOST}/batch/`;
+
 CrUXApiUtil.query = function (requestBody) {
   if (CrUXApiUtil.API_KEY == '[YOUR_API_KEY]') {
     throw 'Replace "YOUR_API_KEY" with your private CrUX API key. Get a key at https://goo.gle/crux-api-key.';
   }
   return fetch(CrUXApiUtil.API_ENDPOINT, {
+    method: 'POST',
+    body: JSON.stringify(requestBody)
+  }).then(response => response.json()).then(response => {
+    if (response.error) {
+      return Promise.reject(response);
+    }
+    return response;
+  });
+};
+
+CrUXApiUtil.queryHistory = function (requestBody) {
+  if (CrUXApiUtil.API_KEY == '[YOUR_API_KEY]') {
+    throw 'Replace "YOUR_API_KEY" with your private CrUX API key. Get a key at https://goo.gle/crux-api-key.';
+  }
+  return fetch(CrUXApiUtil.HISTORY_API_ENDPOINT, {
     method: 'POST',
     body: JSON.stringify(requestBody)
   }).then(response => response.json()).then(response => {
